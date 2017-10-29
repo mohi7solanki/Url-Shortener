6bd05904c04215493dec233de05614066167f283
Mohit <mohitsolanki619@gmail.com>
2017-10-29 13:41:49 +0530

    bug fixes

--- shortener/utils.py
+++ shortener/utils.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-def generate_short_url(size=6, chars=string.ascii_lowercase + string.digits):
+def generate_short_url(size=6, chars=string.ascii_lowercase + string.digits + string.ascii_uppercase):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 
